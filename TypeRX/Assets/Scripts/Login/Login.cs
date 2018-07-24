ecd6dda4acdbf7ef03817f2afe9b026aeec3c490
= <kamaljohnson12345@gmail.com>
2018-07-25 05:16:50 +0530

    added lobby, chat, game apps in django

--- TypeRX/Assets/Scripts/Login/Login.cs
+++ TypeRX/Assets/Scripts/Login/Login.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Login : MonoBehaviour {
 
@@ -10,8 +11,30 @@ public class Login : MonoBehaviour {
      * and get the profile of the user from the server
      * set is_online flag to true in the db
      */
-    void GameLogin()
-    {
 
+    public Text user_name_text;
+    public Text password_text;
+    public Button SubmitButton;
+
+    private void Start()
+    {
+        SubmitButton.onClick.AddListener(GameLogin);
     }
+    private string user_name;
+    private string password;
+
+    public void GameLogin()
+    {
+        Debug.Log("checking. . .");
+        user_name = user_name_text.text;
+        password = user_name_text.text;
+
+        //checking if fields blank
+        if (user_name != "" && password != "")
+        {
+            var response = new WebClient().DownloadString("127.0.0.1:8000/data");
+            Debug.Log(response);
+        }
+    }
+    
 }
