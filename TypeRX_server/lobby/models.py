b1c5f5178313bdf089af006b1a0015c64554d5ec
kamaljohnson <kamaljohnson12345@gmail.com>
2018-07-25 19:53:51 +0530

    added Player model

--- TypeRX_server/lobby/models.py
+++ TypeRX_server/lobby/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+# Player
+# Login
+#   username
+#   password
+# Profile
+#   global_rank
+#   matches_won
+#   type_speed
+#   letters_typed
+# Game
+#   status (Playing, Lobby, Offline)
+#   local_rank
+# region Player Model
+class Player(models.Model):
+    GAME_STATUS = (
+        ('P', 'Playing'),
+        ('L', 'Lobby'),
+        ('O', 'Offline'),
+    )
+
+    # Login fields
+    username = models.CharField(max_length=10, primary_key=True)
+    password = models.CharField(max_length=10)
+
+    # Profile fields
+    global_rank = models.IntegerField(unique=True)
+    matches_won = models.IntegerField()
+    type_speed = models.IntegerField()
+    letters_typed = models.IntegerField()
+
+    # Game fields
+    status = models.CharField(max_length=10, choices=GAME_STATUS)
+    local_rank = models.IntegerField()
+
+# endregion
