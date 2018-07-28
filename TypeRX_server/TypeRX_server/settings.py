c80b54f7b9cdc8872ff2d1366db022c269a263dd
= <kamaljohnson12345@gmail.com>
2018-07-28 18:34:59 +0530

    configuring channels for WebSockets which would be used for chat and game flow

--- TypeRX_server/TypeRX_server/settings.py
+++ TypeRX_server/TypeRX_server/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'lobby',
     'chat',
     'game',
+    'channels',
+
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Channels configs
+ASGI_APPLICATION = "TypeRX_server.routing.application"
---
+++ TypeRX_server/chat/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from .views import *
+urlpatterns = [
+    url(r'^login$', PlayerView.as_view(), name="lobby-login"),
+    url(r'^feeds$', FeedView.as_view(), name="lobby-feed"),
+    url(r'^update-player$', UpdatePlayerView.as_view(), name="lobby-update_player")
+]
